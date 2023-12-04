7c9c1a316fbc387e4a31a1dac35ed55a6ed19059
Ara <joara9566@naver.com>
2023-12-04 17:12:31 +0900

    refactor: 추천정보 컨트롤러 이름 변경

--- src/main/java/com/ara/todayoutfit/recommendinfo/controller/api/RecommendInfoApiController.java
+++ src/main/java/com/ara/todayoutfit/recommendinfo/controller/api/RecommendInfoApiController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class RecommendInfoController {
+public class RecommendInfoApiController {
 
     private final RecommendInfoService recommendInfoService;
 
